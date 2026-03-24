6966b577eec2d8ce6e09c9fc802a52451706a304
Hau99082005 <hau99082005@gmail.com>
2026-03-24 15:35:59 +0700

    main

--- frontend/lib/view/screens/mai_screen.dart
+++ frontend/lib/view/screens/mai_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MaiScreen extends StatefulWidget {
   const MaiScreen({super.key});
@@ -12,11 +12,34 @@ class _MaiScreenState extends State<MaiScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(
-        child: Text('Main Screen', 
-        style: GoogleFonts.roboto(fontSize: 16,
-        fontStyle: FontStyle.normal,),),
-      ))
-    );
+  bottomNavigationBar: Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: BottomNavigationBar(
+      selectedItemColor: Colors.black,       
+      unselectedItemColor: Colors.black54,   
+      type: BottomNavigationBarType.fixed, 
+
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesomeIcons.message),
+          label: 'Đoạn Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesomeIcons.fileLines),
+          label: 'Tin',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesomeIcons.bell),
+          label: 'Thông báo',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesomeIcons.bars),
+          label: 'Menu',
+        ),
+      ],
+    ),
+  ),
+);
+
   }
 }
